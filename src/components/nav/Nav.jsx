41a3ab3b6e6a12d7cad454d3e5a401b43a5049f9
mgazwa <mgazwa@gmail.com>
2022-03-21 14:14:52 +0100

    Navigation banner created

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./nav.css";
+import {AiOutlineHome} from "react-icons/ai";
+import {AiOutlineUser} from "react-icons/ai";
+import {BsBook} from "react-icons/bs";
+import {RiServiceLine} from "react-icons/ri";
+import {BiMessageSquareDetail} from "react-icons/bi";
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#" className='active'><AiOutlineHome/></a>
+            <a href="#about"><AiOutlineUser/></a>
+            <a href="#experience"><BsBook/></a>
+            <a href="#services"><RiServiceLine/></a>
+            <a href="#contact"><BiMessageSquareDetail/></a>
+        </nav>
     )
 }
 
