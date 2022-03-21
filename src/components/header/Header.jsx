c6450d5d39abb29c89783a92f8b51ee93186f30e
mgazwa <mgazwa@gmail.com>
2022-03-21 12:07:05 +0100

    Header created

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/profile.png"
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header_container">
+                <h5>Hello I'm</h5>
+                <h1>Marcin Gązwa</h1>
+                <h5 className="text-light">Junior Frontend Developer</h5>
+                <CTA/>
+                <HeaderSocials/>
+                <div className="me">
+                    <img src={ME} alt="me"/>
+                </div>
+                <a href="#contact" className='scroll_down'>Scroll Down</a>
+
+            </div>
+        </header>
     )
 }
 
