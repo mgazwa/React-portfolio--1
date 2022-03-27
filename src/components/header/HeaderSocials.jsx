9b1ee2f67612fb403abc2acec8ac44570211d8a6
mgazwa <mgazwa@gmail.com>
2022-03-27 21:26:37 +0200

    New CV added

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import {BsFacebook} from "react-icons/bs";
 const HeaderSocials = () => {
     return(
         <div className='header_socials'>
-            <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
+            <a href="https://www.linkedin.com/in/marcin-g%C4%85zwa-628848235/" target="_blank"><BsLinkedin/></a>
             <a href="https://github.com" target="_blank"><BsGithub/></a>
             <a href="https://www.facebook.com/marcin.gazwa" target="_blank"><BsFacebook/></a>
         </div>
